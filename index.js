a28ce375ad11dddc432aea328cb3ddb9a88581bb
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-07 17:17:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,10 +43,9 @@ function total() {
 }
 
 function removeFromCart(item) {
-  let result = "That item is not in your cart.";
   for(let i = 0;i < cart.length;i++) {
     if(cart[i].itemName === item) {
-      result = cart.splice(i, 1);
+       return cart.splice(i, 1);
     } 
   }
 return result; 
